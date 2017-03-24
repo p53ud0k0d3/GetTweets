4bab51555920cf6aef59daf4ec878cf3ed234253
p53ud0k0d3 <thisisvishnuashok@gmail.com>
2017-03-24 12:54:02 +0530

    First Commit

---
+++ get_tweets.py
@@ -0,0 +1,56 @@
+import tweepy
+import csv
+import re
+
+consumer_key = "kEex4FTVUgiv0UbQ9IQ2KNXVS"
+consumer_secret = "9wxktWy7uFg6LVUvf31f9lv7n0WMEDgLn658upiNTb0ZtOt6gB"
+access_key = "2394096084-kCTveFvm2JXsnYSk8wIlR0nOyw1BaZLDLlxrzNP"
+access_secret = "PFWOFw3ZxYj0niXjqornMkLn0m3M6l7zZJ9POJbwLXwlu"
+
+
+def get_all_tweets(screen_name):
+    all_tweets = []
+    new_tweets = []
+    
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    client = tweepy.API(auth)
+    new_tweets = client.user_timeline(screen_name=screen_name, count=200)
+    
+    while len(new_tweets) > 0:
+        for tweet in new_tweets:
+            all_tweets.append(tweet.text.encode("utf-8"))
+        
+        print("We have got %s tweets so far" % (len(all_tweets)))
+        max_id = new_tweets[-1].id - 1
+        new_tweets = client.user_timeline(screen_name=screen_name, count=200, max_id=max_id)
+        
+    return all_tweets
+
+
+def clean_tweet(tweet):
+    tweet = re.sub("https?\:\/\/", "", tweet)   #links
+    tweet = re.sub("#\S+", "", tweet)           #hashtags
+    tweet = re.sub("\.?@", "", tweet)           #at mentions
+    tweet = re.sub("RT.+", "", tweet)           #Retweets
+    tweet = re.sub("Video\:", "", tweet)        #Videos
+    tweet = re.sub("\n", "", tweet)             #new lines
+    tweet = re.sub("^\.\s.", "", tweet)         #leading whitespace
+    tweet = re.sub("\s+", " ", tweet)           #extra whitespace
+    tweet = re.sub("&amp;", "and", tweet)       #encoded ampersands
+    return tweet
+
+
+def write_tweets_to_csv(tweets):
+    with open('tweets.csv', 'wb') as f:
+        writer = csv.writer(f)
+        for tweet in tweets:
+            tweet = clean_tweet(tweet)
+            if tweet:
+                writer.writerow([tweet])
+
+if __name__ == "__main__":
+    tweets = get_all_tweets("iamsrk")
+    write_tweets_to_csv(tweets)
+
+
